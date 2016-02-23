ce17b32f57d94e33f27f7dc74710dbdcb82c731e
Nataraj <nataraj@financialexpress.net>
2016-02-23 19:59:11 +0530

    updated

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
-var accountList = [];
-
-function createAccount(accountObj) {
-    accountList.push(accountObj);
-    return accountObj;
-}
-
-function deposit(username, amount) {
-    accountList.forEach(function (account) {
-        if (account.username === username) {
-            account.balance += amount;
-        }
-    })
-}
-
-function withdraw(username, amount) {
-    accountList.forEach(function (account) {
-        if (account.username === username) {
-            account.balance -= amount;
-        }
-    })
-}
-
-function showAccount(username) {
-    accountList.forEach(function (account) {
-        if (account.username === username) {
-            console.log('User Name: ' + account.username + '\n' + 'Balance: ' + account.balance)
-        }
-    })
-}
-
-var natarajAccount = createAccount({username:'Nataraj', balance:1000});
-var sanahAccount = createAccount({username:'Sanah', balance:2000});
-
-deposit('Nataraj', 1000);
-withdraw('Nataraj', 100);
-showAccount('Nataraj');
-deposit('Sanah', 3000);
-showAccount('Sanah');
-
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 5000;
+app.get('/', function (req, res) {
+    res.send('Bank Account Basic!');
+});
+app.listen(port);
